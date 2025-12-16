2c929181c8decaa613216038e67d2b248929c573
Souvik364 <geetawisdom1@gmail.com>
2025-12-17 01:31:06 +0530

    Update translations.py

--- utils/translations.py
+++ utils/translations.py
@@ -1,3 +1,5 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 """
 Multi-language translations for YouTube Premium Bot
 Supports: English, Bengali (বাংলা), Hindi (हिन्दी)
@@ -319,7 +321,6 @@ def get_text(lang: str, key: str, *args) -> str:
 
 def get_language_keyboard():
     """Get inline keyboard for language selection."""
-    from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
     
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
